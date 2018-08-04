0d93cc6bf7e4e96d21a645151165861f178dd342
kujigoji.app <kujigoji.app@gmail.com>
2018-08-04 10:27:33 +0900

    intellij edited

--- intellipse/src/main/java/intellipse/Intellipse.java
+++ intellipse/src/main/java/intellipse/Intellipse.java
@@ -6,8 +6,10 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ToggleButton;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import org.controlsfx.control.SegmentedButton;
 
 public class Intellipse extends Application {
 
@@ -17,8 +19,12 @@ public class Intellipse extends Application {
 		root.setAlignment(Pos.CENTER);
 		Button b = new Button("push");
 		Label l = new Label("hello");
+		SegmentedButton sb = new SegmentedButton();
+		sb.getButtons().addAll(new ToggleButton("a"),new ToggleButton("b"));
 		root.getChildren().add(b);
 		root.getChildren().add(l);
+		root.getChildren().add(sb);
+		sb.getToggleGroup().selectedToggleProperty().addListener((obs,o,n)->l.setText(((ToggleButton) n).getText()));
 		b.addEventHandler(ActionEvent.ACTION, e -> l.setText("world"));
 		primaryStage.setScene(new Scene(root));
 		primaryStage.show();
